0d583a2a9be73ceba1fbc8105d7cbcb0c39f5163
Nathan Winant <nw@exegetic.net>
2017-02-23 19:11:17 -0600

    Fixes comment formatting

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,16 +1,17 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Emacs customizations
-;;
-;; This file should be loaded automatically. If not, add this to .emacs (or whatever):
-;;
-;;   (load-file "~/.emacs.d/init.el")
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   /================================[[  Emacs init.d  ]]=======================================\
+ ;;| 
+ ;;|    Emacs customizations (nwinant)
+ ;;| 
+ ;;|    This file should be loaded automatically. If not, add this to .emacs (or whatever):
+ ;;| 
+ ;;|      (load-file "~/.emacs.d/init.el")
+ ;;| 
+ ;;; \--------------------------------------------------------------------------------------------/
 
 
-;;;;;;;;;;;;;;;;;;;;;;;
-;; Load custom configs
-;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;  [====[  Load custom configs  ]================================================================]
 
 (load-theme 'manoj-dark t)
 (set-face-attribute 'default nil :background "#2e3436") ;; background of tango-dark
@@ -21,10 +22,7 @@
 (load "shell-utils")
 
 
-
-;;;;;;;;;;;;;;;;;;;;;
-;; Load packages
-;;;;;;;;;;;;;;;;;;;;;
+;;  [====[  Load packages  ]======================================================================]
 
 (require 'package)
 ;; (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
@@ -39,39 +37,37 @@
 ;;   markdown-mode
 
 (require 'evil)
-(evil-mode 1)  ;; toggled per-buffer by C-:
+;(evil-mode 1)  ;; toggled per-buffer by C-:
 
 
-
-;;;;;;;;;;;;;;;;;;;;;
-;; Basic gloss
-;;;;;;;;;;;;;;;;;;;;;
+;;  [====[  Misc functions  ]=====================================================================]
 
 (defun bark-at-user (msg)
   (message "%s" (propertize
 		 msg 'face '(:background "orange" :foreground "black"))))
 
 
+;;  [====[  Mouse stuff  ]========================================================================]
 
-;;;;;;;;;;;;;;;;;;;;;
-;; Mouse stuff
-;;;;;;;;;;;;;;;;;;;;;
+; http://stackoverflow.com/questions/13036155/how-to-combine-emacs-primary-clipboard-copy-and-paste-behavior-on-ms-windows
 
-(setq select-active-regions   t)
-(setq mouse-drag-copy-region  t)
-(setq x-select-enable-primary nil)
+;; (setq select-active-regions   nil)
+;; (setq mouse-drag-copy-region  t)
+;; (setq x-select-enable-primary t)
+;; (setq x-select-enable-clipboard nil)
+
+;;(setq select-active-regions nil)
+;;(setq mouse-drag-copy-region t)
+;;(global-set-key [mouse-2] 'mouse-yank-at-click)
 
 
-
-;;;;;;;;;;;;;;;;;;;;;
-;; Custom BINDINGS
-;;;;;;;;;;;;;;;;;;;;;
-;;
-;; *  C-shift-F1  :  Launch new shell
-;; *  C-shift-F3  :  Cycle through shells, or create one if there are none.
-;; *  C-=         :  Run visual diff on current directory (default tool is meld)
-;; *  C-:         :  Evil mode (toggle)
-;;
+;;  [====[  Custom bindings  ]====================================================================]
+ ;
+ ;|  *  C-shift-F1  :  Launch new shell
+ ;|  *  C-shift-F3  :  Cycle through shells, or create one if there are none.
+ ;|  *  C-=         :  Run visual diff on current directory (default tool is meld)
+ ;|  *  C-:         :  Evil mode (toggle)
+ ;;
 (global-set-key (kbd "C-<f1>") 'run-new-shell)
 (global-set-key (kbd "C-<f3>") 'alt-shell-dwim)
 (add-hook 'shell-mode-hook 'run-shell-init)
@@ -79,43 +75,38 @@
 (global-set-key (kbd "C-?") 'show-cheatsheet)
 (global-set-key (kbd "C-:") 'evil-local-mode)
 
-;; This drives me nuts, so let's disable it:
+;;  This drives me nuts, so let's disable it:
 (global-set-key (kbd "C-x C-c")
 		(lambda ()
 		  (interactive)
-		  (bark-at-user "NOPE! To quit: M-x kill-emacs")))
+		  (bark-at-user "NOPE! To quit: M-x kill-emacs (or :q in evil-mode)")))
+
+
+;;  [====[  Shortcuts  ]==========================================================================]
+ ;
+ ;|  *  M-RET  :  Re-sync dirs between shell & emacs via shell-resync-dirs
+ ;|  *  C-l    :  Move the screen wrt to the cursor, cycling between current line at bottom, middle, 
+ ;|              and top of the screen
+ ;;
+
+
+;;  [====[  OTHER  ]==============================================================================]
+ ;
+ ;|  *  M-x eval-buffer  :  Reload current buffer (e.g., this config)
+ ;|  *  C-u M-x shell    :  Create new shell, prompting for name (e.g, *shell*<2>)
+ ;|  *  C-c C-c p        :  Runs Markdown on the current buffer & displays preview in browser
+ ;|  *  C-c C-c m        :  Runs Markdown on the current buffer & shows HTML output in another buffer
+ ;|  *  C-c C-h          :  List of Markdown-mode key bindings
+ ;;
+
+
+;;  [====[  Emacs daemon, etc.  ]=================================================================]
+ ;
+ ;|  kill-emacs               emacsclient -e '(kill-emacs)'
+ ;|  client-save-kill-emacs   emacsclient -e '(client-save-kill-emacs)'
+ ;;
 
 
 
-;;;;;;;;;;;;;;;;;;;;;
-;; SHORTCUTS
-;;;;;;;;;;;;;;;;;;;;;
-;;
-;; *  M-RET  :  Re-sync dirs between shell & emacs via shell-resync-dirs
-;; *  C-l    :  Move the screen wrt to the cursor, cycling between current line at bottom, middle, 
-;;              and top of the screen
 
-
-
-;;;;;;;;;;;;;;;;;;;;;
-;; OTHER
-;;;;;;;;;;;;;;;;;;;;;
-;;
-;; *  M-x eval-buffer  :  Reload current buffer (e.g., this config)
-;; *  C-u M-x shell    :  Create new shell, prompting for name (e.g, *shell*<2>)
-;; *  C-c C-c p        :  Runs Markdown on the current buffer & displays preview in browser
-;; *  C-c C-c m        :  Runs Markdown on the current buffer & shows HTML output in another buffer
-;; *  C-c C-h          :  List of Markdown-mode key bindings
-
-
-
-
-;;;;;;;;;;;;;;;;;;;;;
-;; Emacs daemon, etc.
-;;;;;;;;;;;;;;;;;;;;;
-;;
-;; kill-emacs               emacsclient -e '(kill-emacs)'
-;; client-save-kill-emacs   emacsclient -e '(client-save-kill-emacs)'
-
-
-
+;;;   \================================[[  ! END Emacs init.d !  ]]===============================/
