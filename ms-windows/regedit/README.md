4a745838a5da42d9d54a11336426945277ab68b0
Nathan Winant <nw@exegetic.net>
2018-08-28 20:18:45 -0500

    Add Windows CAPSLOCK -> CTRL regedit script

---
+++ ms-windows/regedit/README.md
@@ -0,0 +1,8 @@
+MS Windows: Registry & setup scripts
+====================================
+
+Scripts that are intended to be run only once, to set up a system or user account; e.g. Windows
+Registry edits.
+
+Header comments in individual scripts may contain additional information.
+
---
+++ ms-windows/regedit/map-capslock-to-control.ps1
@@ -0,0 +1,8 @@
+##||  Maps CAPS LOCK key to CONTROL
+ #||  Run as Administrator and restart machine
+ ###
+
+$hexified = "00,00,00,00,00,00,00,00,02,00,00,00,1d,00,3a,00,00,00,00,00".Split(',') | % { "0x$_"};
+$kbLayout = 'HKLM:\System\CurrentControlSet\Control\Keyboard Layout';
+New-ItemProperty -Path $kbLayout -Name "Scancode Map" -PropertyType Binary -Value ([byte[]]$hexified);
+
